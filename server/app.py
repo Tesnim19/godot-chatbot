9c041237af0b22d8e5fe4f60578dd04a96940bb7
DeazStar <deadfromtheborn@gmail.com>
2025-03-11 09:06:37 +0300

    add an endpoint to setup api key and check api key

--- server/app.py
+++ server/app.py
@@ -8,21 +8,65 @@ import os
 from fastapi.responses import JSONResponse, Response
 from pydantic import BaseModel
 import logging
+import requests
 
 app = FastAPI()
+
+project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+def setup_config():
+    if not os.path.exists(f"{project_path}/server/config"):
+        os.makedirs(f"{project_path}/server/config")
+        with open(f"{project_path}/server/config/api_key.json", "w") as f:
+            json.dump({"GOOGLE_API_KEY": "not-set"}, f)
+
+def load_api_key():
+    api_key_file = project_path + '/server/config/api_key.json'
+    with open(api_key_file) as f:
+        api_key = json.load(f)
+    
+    os.environ["GOOGLE_API_KEY"] = api_key.get("GOOGLE_API_KEY")
+
+setup_config()
+load_api_key()
 
 manager = ConnectionManager()
 
 agent = AIAgent(model_type="gemini")
-project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 app.mount("/public", StaticFiles(directory=f'{project_path}/server/public/'), name="public")
+
+
+def validate_api_key(api_key):
+    API_VERSION  = "v1"
+    # check if the api key is valid
+    gemini_api = f'https://generativelanguage.googleapis.com/{API_VERSION}/models/gemini-2.0-flash?key={api_key}'
+    try:
+        res = requests.get(gemini_api)
+        print(res.status_code)
+        if res.status_code == 200:
+            return True
+        else:
+            return False
+    except Exception as e:
+        raise Exception(f"Error validating the API")
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
     print('connected')
+    
+    # check if the api key is empty or set to not-set
+    if os.environ.get("GOOGLE_API_KEY") == None or os.environ.get("GOOGLE_API_KEY") == "not-set":
+         await manager.send(json.dumps({'status': 'Failure', "message": "API key not set"}))
+    
+    GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
     try:
+        is_valid = validate_api_key(GOOGLE_API_KEY)
+        if is_valid:
+            await manager.send(json.dumps({"status": "Failure", "message": "API key is valid"}))
+        else:
+            await manager.send(json.dumps({"status": "Success", "message": "API key is invalid"}))
         while True:
             data = await websocket.receive_text()
             # Parse the incoming message as JSON
@@ -106,3 +150,56 @@ async def delete_document(request: DocumentDeleteRequest, response: Response):
         logging.info(f"Error deleting a file {e}")
         response.status_code = 500
         return {"error": f"Error deleting a file {e}"}
+
+@app.get("/check")
+async def check_api_key(response: Response):
+    # check if the api key is empty or set to not-set
+    if os.environ.get("GOOGLE_API_KEY") == None or os.environ.get("GOOGLE_API_KEY") == "not-set":
+         response.status_code = 404
+         return {"message": "API key not set"}
+    
+    GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+    try:
+        is_valid = validate_api_key(GOOGLE_API_KEY)
+        if is_valid:
+            response.status_code = 200
+            return {"message": "API key is valid"}
+        else:
+            response.status_code = 401
+            return {"message": "API key is invalid"}
+    except Exception as e:
+        response.status_code = 500
+        return {"error": str(e)}
+    
+class APIKEYRequest(BaseModel):
+    api_key: str
+
+@app.post("/api_key")
+async def set_api_key(request: APIKEYRequest, response: Response):
+    api_key = request.api_key
+    if api_key == None:
+        response.status_code = 400
+        return {"error": "API key is invalid"}
+    
+    try:
+        is_valid = validate_api_key(api_key)
+        if is_valid:
+            os.environ["GOOGLE_API_KEY"] = api_key
+            api_key_file = project_path + '/server/config/api_key.json'
+            with open(api_key_file) as f:
+                api_key_json = json.load(f)
+                
+            api_key_json["GOOGLE_API_KEY"] = api_key
+            
+            with open(api_key_file, "w") as f:
+                json.dump(api_key_json, f)
+    
+            response.status_code = 200
+            return {"message": "API key set successfully"}
+        else:
+            response.status_code = 401
+            return {"message": "API key is invalid"}
+    except Exception as e:
+        response.status_code = 500
+        return {"error": str(e)}
+    
