3f4c5c04d695c0f900c738a2f1511130bfd44dce
DeazStar <deadfromtheborn@gmail.com>
2025-03-26 10:40:38 +0300

    update build

--- server/build.sh
+++ server/build.sh
@@ -1,9 +1,42 @@
+#!/bin/bash
 echo "Building server as an executable"
-source .venv/bin/activate
-pyinstaller --hidden-import=pydantic.deprecated.decorator \
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Building For Linux"
+    source .venv/bin/activate
+    pyinstaller --distpath dist/linux \
+            --workpath build/linux \
+            --specpath spec/linux \
+            --hidden-import=pydantic.deprecated.decorator \
+            --hidden-import=app \
+            --collect-all chromadb \
+            main.py
+            
+    mkdir dist/linux/main/_internal/server
+    mkdir dist/linux/main/_internal/server/public
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Building for MacOS"
+    source .venv/bin/activate
+    pyinstaller --distpath dist/mac \
+                --workpath build/mac \
+                --specpath spec/mac \
+                --hidden-import=pydantic.deprecated.decorator \
+                --hidden-import=app \
+                --collect-all chromadb \
+                main.py
+
+    mkdir dist/mac/main/_internal/server
+    mkdir dist/mac/main/_internal/server/public
+elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    echo "Building for Windows"
+    pyinstaller --distpath dist/windows \
+            --workpath build/windows \
+            --specpath spec/windows \
+            --hidden-import=pydantic.deprecated.decorator \
             --hidden-import=app --hidden-import=pydantic.deprecated.decorator \
             --collect-all chromadb \
             main.py
-echo "Creating server director and public directory"
-mkdir dist/main/_internal/server
-mkdir dist/main/_internal/server/public
+else
+    echo "Unknown OS"
+    echo "Build Failed"
+fi
